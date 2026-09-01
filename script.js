bb1f3e6c2a9eab821bea87c1b5c7ff14582d76aa
Saumyadeep Mishra <sdm.cord7@gmail.com>
2026-09-02 02:31:06 +0530

    Basic HTML structure

---
+++ script.js
@@ -0,0 +1,17 @@
+const form = document.getElementById("search-form");
+const input = document.getElementById("username-input");
+const statusEl = document.getElementById("status");
+const spinner = document.getElementById("spinner");
+const result = document.getElementById("result");
+const repoList = document.getElementById("repo-list");
+ 
+const avatar = document.getElementById("avatar");
+const nameEl = document.getElementById("name");
+const loginEl = document.getElementById("login");
+const bioEl = document.getElementById("bio");
+const followersEl = document.getElementById("followers");
+const followingEl = document.getElementById("following");
+const reposEl = document.getElementById("repos");
+const locationEl = document.getElementById("location");
+const joinedEl = document.getElementById("joined");
+const profileLink = document.getElementById("profile-link");
